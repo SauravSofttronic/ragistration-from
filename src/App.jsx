279ddbc8137e52c439f3cc1b94983dfe4a462247
SauravSofttronic <maraskolhesaurav1312@gmail.com>
2024-07-07 12:43:40 +0530

    regitration from first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import From1 from './Pages/From1'
+import From2 from './Pages/From2'
+
+function App() {
+
+  return (
+    <div>
+      <From2 />
+    </div>
+  )
+}
+
+export default App
